a1adfe246d45d807af8f6a0baea13d919aa1d1b4
Simon Rogers <simon@blue.midnite.co.uk>
2016-02-11 13:49:19 +0000

    Create setHeightAcrossElements.js

---
+++ setHeightAcrossElements.js
@@ -0,0 +1,24 @@
+<script>
+/*
+ * This script will check the height of each element in 
+ * the $elements definition and then set each element as 
+ * defined with that height. This is useful when using 
+ * floated elements. 
+ *
+ */
+ 
+$(window).load( function() { 
+ 
+  // Define the elements you want to check
+  var $elements = $("#parent > div");
+  
+   var heights = $elements.map(function ()
+       {
+           return $(this).height();
+       }).get(),
+   
+   maxHeight = Math.max.apply(null, heights) + 10;
+   $elements.css('min-height',maxHeight);
+   }
+)
+</script>
