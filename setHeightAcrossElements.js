145704f5086676bd52e055749241d25c47d90108
Simon Rogers <webdev@centralblue.co.uk>
2016-08-11 02:37:56 +0100

    {}
    Update to SetHeightAcrossElements

--- setHeightAcrossElements.js
+++ setHeightAcrossElements.js
@@ -5,19 +5,51 @@
  * floated elements. 
  *
  */
- 
-$(window).load( function() { 
- 
-  // Define the elements you want to check
-  var $elements = $("#parent > div");
-  
-   var heights = $elements.map(function ()
-       {
-           return $(this).height();
-       }).get();
-   
-   maxHeight = Math.max.apply(null, heights) + 10;
-   $elements.css('min-height',maxHeight);
-   }
-)
+
+function HeightElements() {
+
+    var __construct = function() {
+        if (! window.jQuery) {
+            alert('jQuery is needed to set Element Heights');
+            return false;
+        }
+    }();
+
+    var heightElementsGroup = new Array();
+
+    $('[same-height-as-others]').each(function() {
+        var attr = $(this).attr('same-height-as-others');
+        if ($.inArray(attr, heightElementsGroup) < 0) {
+            heightElementsGroup.push(attr);
+        }
+    });
+
+    $(heightElementsGroup).each(function(key, value) {
+        setHeightOnElements(value);
+    });
+
+    $(window).resize(function() {
+        $(heightElementsGroup).each(function(key, value) {
+            setHeightOnElements(value);
+        });
+    });
+
+    function setHeightOnElements(attribute) {
+        var $elements = $("[same-height-as-others="+attribute+"]");
+
+        var heights = $elements.map(function ()
+        {
+            return $(this).height();
+        }).get();
+
+        maxHeight = Math.max.apply(null, heights);
+        $elements.css('min-height',maxHeight);
+    }
+}
+
+
+
+
+
+
 
